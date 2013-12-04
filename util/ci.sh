b5c8bb650685d21fa2b1a6cdbe1638eb50601cbe
Yasuhiko Watanabe <ywataywatay@gmail.com>
2013-12-04 16:00:23 +0900

    ci.sh is changed so that we can build with sandbox or without sandbox.

--- util/ci.sh
+++ util/ci.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+# usage: $0 [sandbox]
+# specify sandbox if cabal sandbo build is prefered,
+# otherwise run $0 to build with installed files.
 
-#
+
+if [ "$1" == "sandbox" ] ; then
 # As this file is intended to run on CI server,
 # the script builds dynamodb package inside sandbox and 
 # it may take much time to finish the build, because 
 # dependencies are build from scrach.
 #
-
-cabal sandbox init
-cabal install --only-dependencies
+    cabal clean
+    cabal sandbox init
+    cabal install --only-dependencies
+fi
 
 cabal configure
 cabal build
-#cabal test
+
+if [ -d ./dist ] ; then
+    ./dist/build/jsonTest/jsonTest
+##    ./dist/build
+fi
+
+
 
 
