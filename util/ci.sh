3c5746894f2b8ba75b3d293616b0c35e5c6286df
Yasuhiko Watanabe <ywataywatay@gmail.com>
2013-12-04 09:41:59 +0900

    Comment for ci.sh is added to describe my intention.

--- util/ci.sh
+++ util/ci.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+#
+# As this file is intended to run on CI server,
+# the script builds dynamodb package inside sandbox and 
+# it may take much time to finish the build, because 
+# dependencies are build from scrach.
+#
+
 cabal sandbox init
 cabal install --only-dependencies
 
 cabal configure
 cabal build
-
+#cabal test
 
 
